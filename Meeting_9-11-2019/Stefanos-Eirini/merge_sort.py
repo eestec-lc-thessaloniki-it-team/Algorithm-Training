b3bdd74ee27bafa8a292e859b621ba380788c1f8
Fantomas4 <Fantomas4@users.noreply.github.com>
2019-11-13 12:50:55 +0200

    Fixed the bug that caused the merge_sort algorithm implementation to return false results

--- Meeting_9-11-2019/Stefanos-Eirini/merge_sort.py
+++ Meeting_9-11-2019/Stefanos-Eirini/merge_sort.py
@@ -23,28 +23,41 @@ def merge_arrays(b_arr, c_arr, a_arr):
             j += 1
         k += 1
 
-        if i == b_len:
-            for v in range(k, len(a_arr)):
-                print("k: ", k)
-                print("a_arr: ", a_arr)
-                print("b_arr: ", b_arr)
-                print("c_arr: ", c_arr)
-                for elem in c_arr:
-                    a_arr[v] = elem
-                    print("a_arr after change1: ", a_arr)
-        else:
-            for v in range(k, len(a_arr)):
-                for elem in b_arr:
-                    a_arr[v] = elem
-                    print("a_arr after change1: ", a_arr)
+    if i == b_len:
+        print("****MPIKA****")
+        while k < len(a_arr):
+            while j < len(c_arr):
+                a_arr[k] = c_arr[j]
+                j += 1
+                k += 1
+        # for v in range(k, len(a_arr)):
+        #     # print("k: ", k)
+        #     # print("a_arr: ", a_arr)
+        #     # print("b_arr: ", b_arr)
+        #     # print("c_arr: ", c_arr)
+        #     for e in range(j, len(c_arr)):
+        #         a_arr[v] = c_arr[e]
+        #         # print("a_arr after change1: ", a_arr)
+    else:
+        while k < len(a_arr):
+            while i < len(b_arr):
+                a_arr[k] = b_arr[i]
+                i += 1
+                k += 1
+
+        # for v in range(k, len(a_arr)):
+        #     for e in range(i, len(b_arr)):
+        #         a_arr[v] = b_arr[e]
+        #         # print("a_arr after change2: ", a_arr)
 
 
 def merge_sort(a_arr):
     n = len(a_arr)
-    b_arr = []
-    c_arr = []
 
     if n > 1:
+        b_arr = []
+        c_arr = []
+
         middle_index = ceil(n/2 - 1)
         i = 0
 
@@ -57,12 +70,22 @@ def merge_sort(a_arr):
             c_arr.append(a_arr[i])
             i += 1
 
+        print("a_arr is: ", a_arr)
+        print("b_arr is: ", b_arr)
+        print("c_arr is: ", c_arr)
+        print("=========================")
+
+
         merge_sort(b_arr)
         merge_sort(c_arr)
         merge_arrays(b_arr, c_arr, a_arr)
 
-    return a_arr
+        print("a_arr after return is: ", a_arr)
+        print("=========================")
 
 
 # print(merge_sort([5,3,4,0,8]))
-print(merge_sort([12,3,88,51,11,0,4]))
+a_arr = [12,3,88,51,11,0,4]
+# a_arr = [88,2,1,52,66]
+merge_sort(a_arr)
+print(a_arr)
--- Meeting_9-11-2019/Stefanos-Eirini/test.py
+++ Meeting_9-11-2019/Stefanos-Eirini/test.py
@@ -1,3 +1,6 @@
 from math import ceil
 
 print(ceil(37/2))
+
+for i in range(1,len([1,2,3])):
+    print(i)
